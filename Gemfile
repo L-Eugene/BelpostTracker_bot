cd6d767c7cd00b8fc0ccd05eee987bba48e275e0
Eugene Lapeko <eugene_lapeko@epam.com>
2022-05-18 00:20:51 +0300

    Cleanup test database on every testcase

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ source 'https://rubygems.org' do
   gem 'telegram-bot-ruby'
 
   group :test do
+    gem 'database_cleaner'
     gem 'factory_bot'
     gem 'rake'
     gem 'rspec'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 
 require 'English'
 require 'factory_bot'
+require 'database_cleaner'
 
 $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../lib/")
 $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../")
@@ -58,6 +59,14 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
