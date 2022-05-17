f639b90bd7a8dc74e3a7a4f4c12afd9b9e11dab1
Eugene Lapeko <eugene_lapeko@epam.com>
2022-05-18 00:22:11 +0300

    Rubocop fixes

--- spec/factories/track.rb
+++ spec/factories/track.rb
@@ -6,9 +6,9 @@ FactoryBot.define do
   factory :track, class: Belpost::Track do
     number do
       String.new.tap do |str|
-        2.times {str << [*'A'..'Z'].sample }
-        9.times {str << [*'1'..'9'].sample }
-        2.times {str << [*'A'..'Z'].sample }
+        2.times { str << [*'A'..'Z'].sample }
+        9.times { str << [*'1'..'9'].sample }
+        2.times { str << [*'A'..'Z'].sample }
       end
     end
   end
