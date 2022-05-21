57464d11b24be15f15f4cd3307cb0f13a912a80b
Eugene Lapeko <eugene_lapeko@epam.com>
2022-05-21 23:31:56 +0300

    Improve tests for chat

--- spec/unit_test/db/belpost_chat_spec.rb
+++ spec/unit_test/db/belpost_chat_spec.rb
@@ -1,70 +1,76 @@
 # frozen_string_literal: true
 
 describe Belpost::Chat do
-  before :each do
-    @chat = FactoryBot.create(:chat)
-  end
+  let(:chat)  { FactoryBot.create :chat }
+  let(:track) { FactoryBot.create :track }
 
   describe '#full?' do
-    context 'empty chat' do
-      it 'should be false' do
-        @chat.inspect
-        expect(@chat.full?).to be_falsey
-      end
+    it 'should be false for empty chat' do
+      chat.inspect
+      expect(chat.full?).to be_falsey
     end
 
-    context 'full chat' do
-      it 'should be true' do
-        Belpost::Chat::TRACK_LIMIT.times { @chat.add FactoryBot.create :track }
+    it 'should be true for full chat' do
+      Belpost::Chat::TRACK_LIMIT.times { chat.add FactoryBot.create :track }
 
-        expect(@chat.full?).to be_truthy
-      end
+      expect(chat.full?).to be_truthy
     end
   end
 
   describe '#add' do
-    context 'empty chat' do
-      it 'shouldn\'t raise on new track' do
-        track = FactoryBot.create :track
-        expect { @chat.add track }.not_to raise_error
-      end
-
-      it 'should raise on duplicate track' do
-        track = FactoryBot.create :track
-        expect { @chat.add track }.not_to raise_error
-        expect { @chat.add track }.to raise_error(Belpost::Error)
-      end
+    it 'shouldn\'t raise on new track' do
+      expect { chat.add track }.not_to raise_error
     end
 
-    context 'full chat' do
-      it 'should raise on #add' do
-        Belpost::Chat::TRACK_LIMIT.times { @chat.add FactoryBot.create :track }
+    it 'should raise on duplicate track' do
+      expect { chat.add track }.not_to raise_error
+      expect { chat.add track }.to raise_error(Belpost::Error)
+    end
 
-        track = FactoryBot.create :track
-        expect { @chat.add track }.to raise_error(Belpost::Error)
-      end
+    it 'should raise on #add if chat is full' do
+      Belpost::Chat::TRACK_LIMIT.times { chat.add FactoryBot.create :track }
+
+      expect { chat.add track }.to raise_error(Belpost::Error)
     end
   end
 
   describe '#watching?' do
-    before(:each) do
-      @track = FactoryBot.create :track
-
-      @chat.add FactoryBot.create :track
-      @chat.add @track
-      @chat.add FactoryBot.create :track
+    before :each do
+      chat.add FactoryBot.create :track
+      chat.add track
+      chat.add FactoryBot.create :track
     end
 
-    it 'should return true for valid track' do
-      expect(@chat.__send__(:watching?, @track)).to be_truthy
+    it 'should return true for valid watched track' do
+      expect(chat.__send__(:watching?, track)).to be_truthy
+    end
+
+    it 'should return false for valid unwatched track' do
+      expect(chat.__send__(:watching?, FactoryBot.create(:track))).to be_falsey
     end
 
     it 'should return false for invalid track' do
-      expect(@chat.__send__(:watching?, FactoryBot.create(:track))).to be_falsey
+      expect(chat.__send__(:watching?, FactoryBot.create(:track))).to be_falsey
     end
 
     it 'should return false for nil object' do
-      expect(@chat.__send__(:watching?, nil)).to be_falsey
+      expect(chat.__send__(:watching?, nil)).to be_falsey
+    end
+  end
+
+  describe '#unwatch' do
+    before :each do
+      chat.add track
+    end
+
+    it 'should remove watched track' do
+      expect { chat.unwatch track }.not_to raise_error
+      expect(chat.tracks.size).to be_zero
+    end
+
+    it 'should remove watched track' do
+      expect { chat.unwatch FactoryBot.create(:track) }.to raise_error(Belpost::Error)
+      expect(chat.tracks.size).to be 1
     end
   end
 end
